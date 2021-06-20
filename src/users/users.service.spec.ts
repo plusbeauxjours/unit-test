a0def17ea74c7783f60bfc97366699b614533b9e
plusbeauxjours <plusbeauxjours@gmail.com>
2021-06-20 19:11:54 +0700

    set: mocking 1/2

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,19 +1,59 @@
 import { Test } from '@nestjs/testing';
-import { UserService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { User } from './entities/user.entity';
+
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { UserService } from 'src/users/users.service';
+import { Verification } from 'src/users/entities/verification.entity';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UserService', () => {
-  // let service: UserService;
+  let service: UserService;
 
-  // beforeAll(async () => {
-  //   const module = await Test.createTestingModule({
-  //     providers: [UserService],
-  //   }).compile();
-  //   service = module.get<UserService>(UserService);
-  // });
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
+    }).compile();
+    service = module.get<UserService>(UserService);
+  });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 
   it.todo('createAccount');
   it.todo('login');
