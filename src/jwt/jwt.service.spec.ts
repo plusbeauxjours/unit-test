e65e3509bb7c850a6a29e2f5ff3d7a54f2d751f5
plusbeauxjours <plusbeauxjours@gmail.com>
2021-06-26 23:24:59 +0700

    set: jwtService Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,14 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+    verify: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -25,6 +33,14 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sign');
-  it.todo('verify');
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      service.sign(ID);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+
+  describe('verify', () => {});
 });
