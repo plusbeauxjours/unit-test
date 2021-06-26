407cf28ff952ba16d2932832d11c601dd99ee51c
plusbeauxjours <plusbeauxjours@gmail.com>
2021-06-26 23:41:23 +0700

    set: mailService init

---
+++ src/mail/mail.service.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import got from 'got';
+import * as FormData from 'form-data';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailService } from './mail.service';
+
+jest.mock('got', () => {});
+jest.mock('form-data', () => {
+  return {
+    append: jest.fn(),
+  };
+});
+
+describe('MailService', () => {
+  let service: MailService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MailService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: {
+            apiKey: 'test-apiKey',
+            domai: 'test-domai',
+            fromEmail: 'test-fromEmail',
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<MailService>(MailService);
+  });
+
+  it('should be defiled', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('sendEmail');
+  it.todo('sendVerificationEmail');
+});
